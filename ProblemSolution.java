df3ca93ca58992b9695cf645d03bbe6076fa82bd
Aleevee <124316336+Aleevee@users.noreply.github.com>
2023-04-02 20:52:33 -0500

    Create ProblemSolution.java

---
+++ ProblemSolution.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class ProblemSolution {
+  static void DisplaySum(long N1, long N2) {
+  }
+  public long solution(long N1, long N2) {
+    DisplaySum(N1, N2);
+    return N1 + N2;
+
+  }
+}
