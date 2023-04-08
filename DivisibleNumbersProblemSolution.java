bbf56ccbbe1187c86b0fee32b24cb855d00d2633
Aleevee <124316336+Aleevee@users.noreply.github.com>
2023-04-07 21:16:31 -0500

    Create DivisibleNumbersProblemSolution.java

---
+++ DivisibleNumbersProblemSolution.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class ProblemSolution {
+    public int solution(int N) {
+
+        Scanner s = new Scanner(System.in);
+        int divisor = s.nextInt();
+
+        switch (N/divisor){
+            case 2:
+                System.out.println(N/divisor);
+            case 3:
+                System.out.println(N/divisor);
+            case 6:
+                System.out.println(N/divisor);
+            }
+        return -1;
+    }
+}
