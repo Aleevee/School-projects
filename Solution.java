c65e35c31afb0830cc786c944a04cc02d58dc85b
Aleevee <124316336+Aleevee@users.noreply.github.com>
2023-04-11 21:32:58 -0500

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Solution {
+    static void Student() {
+        int rollNumber = 10001;
+        System.out.println(rollNumber);
+    }
+        public static void main(String[] args) {
+        Student();
+    }
+}
