837ed7414d564da2d37d391b83fa8d12c5908595
Aleevee <124316336+Aleevee@users.noreply.github.com>
2023-04-13 11:21:12 -0500

    Create ObjectCreationSolution.java

---
+++ ObjectCreationSolution.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Solution {
+  public static void main(String[] args) {
+
+    NewStudent newStudent = new NewStudent();
+    System.out.print(newStudent.solution());
+  }
+}
