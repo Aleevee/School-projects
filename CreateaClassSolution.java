e19d947238895c7c9d069a1654d1c5ff63a7aad4
Aleevee <124316336+Aleevee@users.noreply.github.com>
2023-04-13 11:31:20 -0500

    Create CreateaClassSolution.java

---
+++ CreateaClassSolution.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Solution {
+   public static void main(String[] args) {
+    
+     ProblemSolution problemSolution = new ProblemSolution();
+     System.out.println(problemSolution.solution());
+   }
+}
