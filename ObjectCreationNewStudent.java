456669226688e8ae0e7ffaf12ac6fcd31a71e756
Aleevee <124316336+Aleevee@users.noreply.github.com>
2023-04-13 11:19:50 -0500

    Create ObjectCreationNewStudent.java

---
+++ ObjectCreationNewStudent.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class NewStudent{
+  public String solution(){
+
+    String object = "Object created successfully";
+    System.out.println(object);
+    return "display function called successfully";
+  }
+}
