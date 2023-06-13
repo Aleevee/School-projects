95b71f320c2d449f4c43b2f98b7a33522e723142
Aleevee <124316336+Aleevee@users.noreply.github.com>
2023-06-13 12:09:13 -0500

    Create CreateClass.java
    Create a class 'ProblemSolution'  and a method 'solution' inside the class with no input parameter and return type as void. The method should print "HELLO WORLD ".

---
+++ CreateClass.java
@@ -0,0 +1,10 @@
+public class ProblemSolution {
+    public void solution() {
+        System.out.println("HELLO WORLD");
+    }
+
+    public static void main(String[] args) {
+        ProblemSolution problem = new ProblemSolution();
+        problem.solution();
+    }
+}
